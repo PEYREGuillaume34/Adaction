708e4d592ec1bcd3a0202fb4b2dc791e49f7fa9a
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-10-12 18:20:34 +0200

    Adaction

---
+++ README.md
@@ -0,0 +1,25 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/FQw970Td)
+
+# DrawSql
+https://drawsql.app/teams/ada-tech-school-29/diagrams/copy-of-adaction
+
+# Excalidraw schéma du projet
+https://excalidraw.com/#json=Vk9DiZdGhYchYMoUUUFUn,BvYIlsiuiI8WtOkHo7iXZA
+
+
+# 29/09/2025
+- Schématisation de la base de donnée sur DrawSQL
+- Initialisation du serveur
+- Création de la première route GET / Volonteers, qui est directement en lien avec la Data de Néon
+
+# 30/09/2025
+Creation de la route GET / Volunteers / Location
+
+# 1/10/2025
+Page Asso et page Collecte/Type créés (DOM)
+Faire le CRUD des Volunteers
+
+# 2/10/2025
+POST type fait
+Tables modifiées
+Et organisations des tâches
---
+++ front/scriptsjs/fetchs-iris.js
@@ -0,0 +1,82 @@
+//constantes d'identification user
+const username = localStorage.getItem("username");
+const userId = localStorage.getItem("id");
+
+// créer un element dans le dom
+function nouvelleBalise(type, texte, id) {
+    let element = document.createElement(`${type}`);
+    element.innerText = texte;
+    return id.appendChild(element);
+}
+function nouvelleBalise2(type, id) {
+    let element = document.createElement(`${type}`);
+    return id.appendChild(element);
+}
+
+// function fetch sur table volunteers GET
+const getUserInfos = async () => {
+    const userId = localStorage.getItem("id");
+    if (!userId) return null;
+    const response = await fetch(`http://localhost:3000/volunteer/profile/${userId}`);
+    return await response.json();
+};
+
+// function fetch sur table associations GET
+const getAssolist = async () => {
+    const response = await fetch('http://localhost:3000/associations');
+    const data = await response.json();
+    return (data);
+}
+
+// remplit le menu déroulant des associations
+function assoDropMenu(array, input) {
+
+    for (const el of array) {
+        const option = document.createElement('option');
+        option.textContent = el.name;
+        input.appendChild(option);
+        option.value = el.id;
+        option.id = el.id;
+    }
+}
+
+// fonction import des points user
+const fetchUserPoints = async (id) => {
+    try {
+
+        const response = await fetch(`http://localhost:3000/volunteer/${id}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data[0].points;
+
+    } catch (error) {
+        console.error('Error fetching user points:', error);
+        return null;
+    }
+}
+
+// fonction import infos pour historique
+
+const getHistoInfo = async (id) => {
+    const response = await fetch(`http://localhost:3000/collects/volunteer/${id}`);
+    const data = await response.json()
+    return data;
+}
+
+// const effaceLS = () => {
+//     document.addEventListener('click', (event) => {
+//         const headerAdaction = event.target.closest("#logoAdaction");
+//         if (headerAdaction) {
+//             localStorage.clear();
+//         }
+//     })
+// }
+
+
+
+export { getUserInfos, getAssolist, assoDropMenu, fetchUserPoints, getHistoInfo, nouvelleBalise, nouvelleBalise2, username, userId };
